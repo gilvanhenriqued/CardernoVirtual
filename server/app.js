20e0096924655a396ca4bf3f6ae6585fa64298d6
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-10 20:39:28 -0300

    mongodb atlas connection and remove render of routes

--- server/app.js
+++ server/app.js
@@ -1,20 +1,38 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// imports
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+// mogodb configurations
+// pass: 
+mongoose.connect(
+  'mongodb+srv://gilvanh:dBzCET6SJHbJFIhX@development.cefey.mongodb.net/cadernovirtualdb?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Mongoose connection ok!');
+}).catch((err) => {
+  console.log(err);
+});
 
+// app configurations
+const app = express();
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 module.exports = app;
