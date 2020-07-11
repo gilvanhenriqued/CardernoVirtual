479585c85914cace4e2590fd313362feea5065b1
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-11 10:17:49 -0300

    get annotations list route

--- server/models/annotations.js
+++ server/models/annotations.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const Annotations = mongoose.model('Annotation', new mongoose.Schema({
-  author: {type: String, required: true, unique: true},
+  author: {type: String, required: true},
   title: {type: String, required: true},
   description: {type: String, required: true},
   publicationDate: {type: Date},
+  isPublic: {type: Boolean, required: true},
   subjectsTags: [String]
 }));
 
--- server/routes/annotations.js
+++ server/routes/annotations.js
@@ -2,23 +2,39 @@ const express = require('express');
 const router = express.Router();
 const Annotations = require('../models/annotations');
 
-// POST – To create a new annotation (http://localhost:8080/api/mensagens)
+// POST – To create a new annotation (localhost:3000/annotations)
 router.post('/annotations', async (req, res) => {
   const annotation = new Annotations ({
     author: req.body.author,
     title: req.body.title,
     description: req.body.description,
     publicationDate: new Date(),
+    isPublic: req.body.isPublic,
     subjectsTags: req.body.subjectsTags
   });
 
   await annotation.save()
     .then((annotation) => {
-      response(res, true, "Annotation successfully registered!", annotation, 201)
+      response(res, true, "Annotation successfully registered!", annotation, 201);
     }, (error) => {
-      response(res, false, "Failed trying create annotation...", error, 500)
+      response(res, false, "Failed trying create annotation...", error, 500);
     });
-})
+});
+
+// GET – To get the public annotations (localhost:3000/annotations)
+router.get('/annotations', async (req, res) => {
+  await Annotations.find({})
+    .exec()
+    .then((annotations) => {
+      response(res, true, "Annotations listed!", annotations, 200);
+    }, (error) => {
+      response(res, false, "Failed trying list annotations...", error, 500);
+    });
+});
+
+
+
+
 
 // function to optimizate the responses
 function response(res, success=true, msg="", result, status){
